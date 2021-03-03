979bfe0c0dc1449c36edfc38354ccf714a6684b7
meichaoyang <7606538+meichaoyang@user.noreply.gitee.com>
2021-03-03 22:46:26 +0800

    Add eval识别文件

--- eval.py
+++ eval.py
@@ -6,7 +6,7 @@ import time
 import torch
 import torch.nn as nn
 import torch.utils.data
-from rnnt.model import Transducer
+from rnnt.model import Transducer, CTC
 from rnnt.optim import Optimizer
 from rnnt.dataset import AudioDataset
 from tensorboardX import SummaryWriter
@@ -15,7 +15,7 @@ from rnnt.utils import AttrDict, init_logger, count_parameters, save_model, comp
 
 
 
-def eval(config, model, validating_data, logger):
+def eval(epoch, config, model, validating_data, logger, visualizer=None):
     model.eval()
     total_loss = 0
     total_dist = 0
@@ -34,22 +34,26 @@ def eval(config, model, validating_data, logger):
 
         preds = model.recognize(inputs, inputs_length)
 
-
         transcripts = [targets.cpu().numpy()[i][:targets_length[i].item()]
                        for i in range(targets.size(0))]
-        logger.info(preds)
 
         dist, num_words = computer_cer(preds, transcripts)
         total_dist += dist
         total_word += num_words
 
         cer = total_dist / total_word * 100
-
+        if step % config.training.show_interval == 0:
+            process = step / batch_steps * 100
+            logger.info('-Validation-Epoch:%d(%.5f%%), CER: %.5f %%' % (epoch, process, cer))
+            logger.info('preds:'+validating_data.dataset.decode(preds[0], rm_blk=True))
+            logger.info('transcripts:'+validating_data.dataset.decode(transcripts[0]))
 
     val_loss = total_loss/(step+1)
-    logger.info('-Validation-:, AverageLoss:%.5f, AverageCER: %.5f %%' %
-                (val_loss, cer))
+    logger.info('-Validation-Epoch:%4d, AverageLoss:%.5f, AverageCER: %.5f %%' %
+                (epoch, val_loss, cer))
 
+    if visualizer is not None:
+        visualizer.add_scalar('cer', cer, epoch)
 
     return cer
 
@@ -92,58 +96,22 @@ def main():
         torch.manual_seed(config.training.seed)
     logger.info('Set random seed: %d' % config.training.seed)
 
-    model = Transducer(config.model)
-
-    if config.training.load_model:
+    if config.model.type == "transducer":
+        model = Transducer(config.model)
         checkpoint = torch.load(config.training.load_model)
         model.encoder.load_state_dict(checkpoint['encoder'])
         model.decoder.load_state_dict(checkpoint['decoder'])
         model.joint.load_state_dict(checkpoint['joint'])
         logger.info('Loaded model from %s' % config.training.load_model)
-    elif config.training.load_encoder or config.training.load_decoder:
-        if config.training.load_encoder:
-            checkpoint = torch.load(config.training.load_encoder)
-            model.encoder.load_state_dict(checkpoint['encoder'])
-            logger.info('Loaded encoder from %s' %
-                        config.training.load_encoder)
-        if config.training.load_decoder:
-            checkpoint = torch.load(config.training.load_decoder)
-            model.decoder.load_state_dict(checkpoint['decoder'])
-            logger.info('Loaded decoder from %s' %
-                        config.training.load_decoder)
-
-    if config.training.num_gpu > 0:
-        model = model.cuda()
-        if config.training.num_gpu > 1:
-            device_ids = list(range(config.training.num_gpu))
-            model = torch.nn.DataParallel(model, device_ids=device_ids)
-        logger.info('Loaded the model to %d GPUs' % config.training.num_gpu)
-
-    n_params, enc, dec = count_parameters(model)
-    logger.info('# the number of parameters in the whole model: %d' % n_params)
-    logger.info('# the number of parameters in the Encoder: %d' % enc)
-    logger.info('# the number of parameters in the Decoder: %d' % dec)
-    logger.info('# the number of parameters in the JointNet: %d' %
-                (n_params - dec - enc))
-
-    optimizer = Optimizer(model.parameters(), config.optim)
-    logger.info('Created a %s optimizer.' % config.optim.type)
-
-    if opt.mode == 'continue':
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        start_epoch = checkpoint['epoch']
-        logger.info('Load Optimizer State!')
+    elif config.model.type == "ctc":
+        model = CTC(config.model)
+        checkpoint = torch.load(config.training.load_encoder)
+        model.encoder.load_state_dict(checkpoint['encoder'])
+        model.encoder.load_state_dict(checkpoint['project_layer'])
+        logger.info('Loaded encoder from %s' %
+                    config.training.load_encoder)
     else:
-        start_epoch = 0
-
-    # create a visualizer
-    if config.training.visualization:
-        visualizer = SummaryWriter(os.path.join(exp_name, 'log'))
-        logger.info('Created a visualizer.')
-    else:
-        visualizer = None
-
-
+        raise NotImplementedError
 
 
     _ = eval(config, model, validate_data, logger)
