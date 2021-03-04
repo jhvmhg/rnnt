0b80198f6d2e9732614e050d38c959112ee64ff8
meichaoyang <7606538+meichaoyang@user.noreply.gitee.com>
2021-03-04 15:01:28 +0800

    Add _collate_fn

--- eval.py
+++ eval.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.utils.data
 from rnnt.model import Transducer, CTC
 from rnnt.optim import Optimizer
-from rnnt.dataset import AudioDataset
+from rnnt.dataset import AudioDataset, _collate_fn
 from tensorboardX import SummaryWriter
 from rnnt.utils import AttrDict, init_logger, count_parameters, save_model, computer_cer
 
@@ -27,10 +27,10 @@ def eval(config, model, validating_data, logger, visualizer=None):
             inputs, inputs_length = inputs.cuda(), inputs_length.cuda()
             targets, targets_length = targets.cuda(), targets_length.cuda()
 
-        max_inputs_length = inputs_length.max().item()
-        max_targets_length = targets_length.max().item()
-        inputs = inputs[:, :max_inputs_length, :]
-        targets = targets[:, :max_targets_length]
+        # max_inputs_length = inputs_length.max().item()
+        # max_targets_length = targets_length.max().item()
+        # inputs = inputs[:, :max_inputs_length, :]
+        # targets = targets[:, :max_targets_length]
 
         preds = model.recognize(inputs, inputs_length)
 
@@ -82,7 +82,7 @@ def main():
     dev_dataset = AudioDataset(config.data, 'dev')
     validate_data = torch.utils.data.DataLoader(
         dev_dataset, batch_size=config.data.batch_size * config.training.num_gpu if config.training.num_gpu>0 else config.data.batch_size,
-        shuffle=False, num_workers=num_workers)
+        shuffle=False, num_workers=num_workers, collate_fn=_collate_fn)
     logger.info('Load Dev Set!')
 
     if config.training.num_gpu > 0:
