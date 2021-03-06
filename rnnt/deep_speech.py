57a77f3c9d885ac5b8ee94c74699b93cbbc86147
meichaoyang <7606538+meichaoyang@user.noreply.gitee.com>
2021-03-06 15:06:00 +0800

    初步完成deep speech训练的整合，并FIx配置文件，下一步进行测试

--- rnnt/deep_speech.py
+++ rnnt/deep_speech.py
@@ -135,7 +135,7 @@ class DeepSpeech(nn.Module):
             nn.Hardtanh(0, 20, inplace=True)
         ))
         # Based on above convolutions and spectrogram size using conv formula (W - F + 2P)/ S+1
-        rnn_input_size = input_size*320
+        rnn_input_size = input_size*8
 
         self.rnns = nn.Sequential(
             BatchRNN(
